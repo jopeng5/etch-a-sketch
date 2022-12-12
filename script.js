90c89c95bcfd9cb19b4617963455d4ee9238795c
jopeng5 <jopeng5@users.noreply.github.com>
2022-12-11 23:51:12 -0800

    Create container for grid

---
+++ script.js
@@ -0,0 +1,8 @@
+// select container div
+const container = document.querySelector('.container');
+
+// create div inside container with class cell
+const cell = document.createElement('div');
+cell.classList.add('cell');
+
+
