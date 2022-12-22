f245d5ede18e342fb8415b5cea302acbe22d8b3c
jopeng5 <jopeng5@users.noreply.github.com>
2022-12-21 22:00:03 -0800

    Add function to get grid size

--- script.js
+++ script.js
@@ -2,30 +2,40 @@
 const contentDiv = document.querySelector('.content');
 
 // listen for click on button 16 x 16
-const btnGrid16 = document.querySelector('.btn-grid-16');
-let num =
-btnGrid16.addEventListener('click', createDivs())
+// const btnGrid16 = document.querySelector('.btn-grid-16');
+// let num =
+// btnGrid16.addEventListener('click', createDivs())
 
-// function to create child divs inside content div
-function createDivs(num) {
-  for (i = 0; i < num; i++) {
-    const gridItem = document.createElement('div');
-    contentDiv.appendChild(gridItem);
-    gridItem.classList.add('grid-item');
-  }
-}
+// add click event to 16 x 16 button and return grid size
+const btn16 = document.querySelector('.btn-16');
 
-// change div color when mouseover
-function changeDivColor() {
-  const gridItem = document.querySelectorAll('.grid-item');
-  gridItem.forEach(function(item) {
-    item.addEventListener('mouseover', function() {
-      item.classList.add('hover');
-      });
-  });
+function getGridSize() {
+  console.log(parseInt(btn16.textContent.slice(0,2),16)); 
 };
 
-createDivs(numOfDivs);
-changeDivColor();
+btn16.addEventListener('click', getGridSize);
+
+
+// // function to create child divs inside content div
+// function createDivs(num) {
+//   for (i = 0; i < num; i++) {
+//     const gridItem = document.createElement('div');
+//     contentDiv.appendChild(gridItem);
+//     gridItem.classList.add('grid-item');
+//   }
+// }
+
+// // change div color when mouseover
+// function changeDivColor() {
+//   const gridItem = document.querySelectorAll('.grid-item');
+//   gridItem.forEach(function(item) {
+//     item.addEventListener('mouseover', function() {
+//       item.classList.add('hover');
+//       });
+//   });
+// };
+
+// createDivs(numOfDivs);
+// changeDivColor();
 
 
